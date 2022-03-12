5f1776115a6dc063f35b0bcff77848c41852932e
Sarmad Junaid <sarmadjkhan@gmail.com>
2022-03-12 16:53:08 +0500

    query list fixed

--- book_app/models.py
+++ book_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Genre(models.Model):
-    genre = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.genre
@@ -12,7 +12,7 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     writer = models.CharField(max_length=255)
     synopsis = models.TextField(max_length=1000)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
     release_date = models.DateTimeField()
     price = models.FloatField(verbose_name='Price in cents (¢)')
     
